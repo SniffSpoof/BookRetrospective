48d1eafa48f48d9de617ff7aa5bf2c2b9ebd1aa2
SniffSpoof <52812804+SniffSpoof@users.noreply.github.com>
2025-02-10 22:14:45 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,6 +4,11 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Telegram bot settings")
     parser.add_argument('--telegram-token', type=str, required=True, help="Telegram API token")
     parser.add_argument('--gemini-api-keys', nargs='+', required=True, help="List of Gemini API keys")
+
+    parser.add_argument('--gmail-login', type=str, required=False, help="Your Gmail logn")
+    parser.add_argument('--gmail-app-password', type=str, required=False, help="Your app_password")
+    parser.add_argument('--receivers-email', type=str, required=False, help="Receiver's email")
+
     args = parser.parse_args()
 
     if not args.telegram_token or not all(args.gemini_api_keys):
